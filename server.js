826f5317c6d5024d5bc1ae71471a34743b56db44
Kai <1114366961@qq.com>
2020-12-21 18:27:37 +0800

    3.实现后端对数据的接收存储

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ if(!port){
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
-
 var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true)
   var pathWithQuery = request.url 
@@ -21,31 +20,58 @@ var server = http.createServer(function(request, response){
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
-  response.statusCode = 200
-  //默认首页
-  const filePath = path==='/'?'/index.html' : path
-  const index =filePath.lastIndexOf('.')
-  console.log(filePath.lastIndexOf('.'))
-  //suffix是后缀
-  const suffix=filePath.substring(index)
-  console.log(suffix)
-  const fileTypes={
-        '.html':'text/html',
-        '.css':'text/css',
-        '.js':'text/javascript',
-        'png':'image/png',
-        'jpg':'image/jpeg'
-  }
-  response.setHeader('Content-Type', `${fileTypes[suffix] ||'text/html'};charset=utf-8`)
-  let content
-    try{
-        content =fs.readFileSync(`./public${filePath}`)
-    }catch(error){
-        content = '文件不存在'
-        response.statusCode =404
+  if(path==='/register'&& method==='POST'){
+    response.setHeader('Content-Type','text/html;  charset=UTF-8')
+    const userArray = JSON.parse(fs.readFileSync('./db/users.json'))
+    console.log(userArray)
+    const array=[]
+    request.on('data',(chunk)=>{
+      array.push(chunk)
+    })
+    request.on('end',(e)=>{
+      const string = Buffer.concat(array).toString()
+      const obj=JSON.parse(string)
+      const lastUser = userArray[userArray.length-1]
+        const newUser ={
+          //id为最后一个用户的id+1
+          id: lastUser ? lastUser.id+1 :1,
+          name:obj.name,
+          password:obj.password
+        }
+      userArray.push(newUser)
+      console.log(userArray)
+      fs.writeFileSync('./db/users.json',JSON.stringify(userArray))
+      response.end('')
+    })
+
+  }else{
+    response.statusCode = 200
+    //默认首页
+    const filePath = path==='/'?'/index.html' : path
+    const index =filePath.lastIndexOf('.')
+    console.log(filePath.lastIndexOf('.'))
+    //suffix是后缀
+    const suffix=filePath.substring(index)
+    console.log(suffix)
+    const fileTypes={
+          '.html':'text/html',
+          '.css':'text/css',
+          '.js':'text/javascript',
+          'png':'image/png',
+          'jpg':'image/jpeg'
     }
-  response.write(content)
-  response.end()
+    response.setHeader('Content-Type', `${fileTypes[suffix] ||'text/html'};charset=utf-8`)
+    let content
+      try{
+          content =fs.readFileSync(`./public${filePath}`)
+      }catch(error){
+          content = '文件不存在'
+          response.statusCode =404
+      }
+    response.write(content)
+    response.end()
+  }
+
 
 
 
